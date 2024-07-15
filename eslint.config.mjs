51adfafaa5816c35717733d47d29543eecdb9f6d
Miguel Osorio @Kelvur <osorio10.92@gmail.com>
2024-07-15 22:00:56 +0200

    chore: remove the file '.eslintrc' as it's not needed any more

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,12 +8,13 @@ import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+    allConfig: js.configs.all,
 });
 
 export default [{
